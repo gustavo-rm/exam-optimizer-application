b6d0cd163bbabfb2fcac114215ea3df266797fd4
gustavo-rm <gustavomalacarne.r@gmail.com>
2025-08-28 10:30:23 -0300

    feat(api/mapper): create mapper classes

---
+++ src/main/java/com/ia/project/dynamicstudyplanner/api/mapper/ExamMapper.java
@@ -0,0 +1,4 @@
+package com.ia.project.dynamicstudyplanner.api.mapper;
+
+public class ExamMapper {
+}
---
+++ src/main/java/com/ia/project/dynamicstudyplanner/api/mapper/Mapper.java
@@ -0,0 +1,4 @@
+package com.ia.project.dynamicstudyplanner.api.mapper;
+
+public interface Mapper {
+}
